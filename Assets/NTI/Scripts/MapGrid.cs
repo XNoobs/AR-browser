dc39fff33a991d89ac9bf0ef1c0b4a70c91b1e01
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2019-02-27 16:23:46 +0300

    Added placeObject method

--- Assets/NTI/Scripts/MapGrid.cs
+++ Assets/NTI/Scripts/MapGrid.cs
@@ -9,15 +9,19 @@ namespace NTI.Scripts
 {
     public class MapGrid : MonoBehaviour
     {
-        [SerializeField] private uint squareSize = 7;
+        private uint _padding;
         [SerializeField] private GameObject square;
         [SerializeField] private Canvas userInterface;
+        [SerializeField] private GameObject objectToPlace;
         private uint height = 10;
         private uint width = 10;
-        private readonly Color _activeCellColor = Color.red;
+        private float movementX;
+        private float movementZ;
         private InputField _inputHeight;
         private InputField _inputWidth;
+        private InputField _inputSquareSize;
         private GameObject[,] _grid;
+        private bool[,] _cellsVacated;
         private GameObject menuBtn;
 
 
@@ -33,17 +37,17 @@ namespace NTI.Scripts
 
         private void DrawMenuButton(bool UIOn)
         {
-            var padding = 0f;
+            var buttonPos = 0f;
             if (UIOn)
             {
-                padding = -28;
+                buttonPos = -28;
             }
             else
             {
-                padding = -(width / 2 + 1) * squareSize;
+                buttonPos = -(width / 2 + 1) * _padding;
             }
 
-            var position = new Vector3(0, 0, padding);
+            var position = new Vector3(0, 0, buttonPos);
             menuBtn.transform.position = position;
             menuBtn.SetActive(true);
         }
@@ -53,42 +57,44 @@ namespace NTI.Scripts
             UInt32.TryParse(_inputHeight.text, out height);
             UInt32.TryParse(_inputWidth.text, out width);
 
-            float movementX;
-            float movementZ;
+            uint tmp;
+            UInt32.TryParse(_inputSquareSize.text, out tmp);
+            square.transform.localScale = new Vector3(tmp, tmp, 0);
+            _padding = tmp + 1;
 
             _grid = new GameObject[height, width];
+            _cellsVacated = new bool[height, width];
+            
             if (height % 2 == 0)
             {
-                movementX = Convert.ToSingle((height / 2 - 0.5) * squareSize);
+                movementX = Convert.ToSingle((height / 2 - 0.5) * _padding);
             }
             else
             {
-                movementX = height / 2 * squareSize;
+                movementX = height / 2 * _padding;
             }
 
             if (width % 2 == 0)
             {
-                movementZ = Convert.ToSingle((width / 2 - 0.5) * squareSize);
+                movementZ = Convert.ToSingle((width / 2 - 0.5) * _padding);
             }
             else
             {
-                movementZ = width / 2 * squareSize;
+                movementZ = width / 2 * _padding;
             }
-
-
-            square.transform.localScale = new Vector3(5, 5, 0);
 
             for (var i = 0; i < height; i++)
             {
                 for (var j = 0; j < width; j++)
                 {
-                    var position = new Vector3(i * squareSize - movementX, 0, j * squareSize - movementZ);
+                    var position = new Vector3(i * _padding - movementX, 0, j * _padding - movementZ);
                     var current = Instantiate(square, position, Quaternion.Euler(90f, 0f, 0f)) as GameObject;
                     current.SetActive(true);
                     current.transform.SetParent(this.transform);
                     var currentBoxCollider = current.AddComponent<BoxCollider>();
                     currentBoxCollider.name = i.ToString() + '_' + j.ToString();
                     _grid[i, j] = current;
+                    _cellsVacated[i, j] = false;
                 }
             }
         }
@@ -104,15 +110,32 @@ namespace NTI.Scripts
             }
         }
 
+        private void placeObject(GameObject objectToPlace, int[] coordinates, Quaternion rotation, bool vacation)
+        {
+            var position = new Vector3(coordinates[0] * _padding - movementX, 0, coordinates[1] * _padding - movementZ);
+            var current = Instantiate(objectToPlace, position, rotation) as GameObject;
+            current.SetActive(true);
+            current.transform.SetParent(this.transform);
+            var currentBoxCollider = current.AddComponent<BoxCollider>();
+            currentBoxCollider.name = coordinates[0].ToString() + '_' + coordinates[1].ToString();
+            _grid[coordinates[0], coordinates[1]] = current;
+            _cellsVacated[coordinates[0], coordinates[1]] = vacation;
+        }
+        
 
         private void Start()
         {
+            square.transform.localScale = new Vector3(5, 5, 0);
             userInterface.enabled = true;
+            var squareSize = square.GetComponent<SpriteRenderer>().bounds.size;
+            _padding = Convert.ToUInt16(squareSize.x+1);
             CreateMenuButton();
             DrawMenuButton(true);
+            
             //caching objects refs to increase performance
             _inputHeight = GameObject.Find("size_x").GetComponent<InputField>();
             _inputWidth = GameObject.Find("size_z").GetComponent<InputField>();
+            _inputSquareSize = GameObject.Find("squareSize").GetComponent<InputField>();
         }
 
         private void Update()
@@ -130,19 +153,20 @@ namespace NTI.Scripts
                         {
                             var name = raycastHit.collider.name.Split('_');
                             int[] coordinates = {Convert.ToInt16(name[0]), Convert.ToInt16(name[1])};
-                            var sprite = _grid[coordinates[0], coordinates[1]].GetComponent<SpriteRenderer>();
-                            if (sprite.color != _activeCellColor)
+                            var cellHitted = _grid[coordinates[0], coordinates[1]];
+                            if (_cellsVacated[coordinates[0],coordinates[1]] == false)
                             {
-                                sprite.color = _activeCellColor;
+                                placeObject(objectToPlace, coordinates, Quaternion.Euler(0f,0f,0f), true);
+                                Destroy(cellHitted);
                             }
                             else
                             {
-                                var tmpColor = square.GetComponent<SpriteRenderer>().color;
-                                sprite.color = tmpColor;
+                                placeObject(square,coordinates, Quaternion.Euler(90f,0f,0f), false);
+                                Destroy(cellHitted);
                             }
+
                         }
                     }
-
                     if (raycastHit.collider.name == "menu")
                     {
                         if (userInterface.enabled == false)
